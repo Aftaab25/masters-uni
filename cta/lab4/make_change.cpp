e2368899babe840698b5ad97378cab9f5d2f09d4
aftaab25 <aftaab2507@gmail.com>
2024-09-17 17:07:14 +0530

    Make Change Greedy for prac4

---
+++ cta/lab4/make_change.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define nline "\n";
+
+void printArr(vector<int> v) {
+    for (int i: v) {
+        cout << i << " ";
+    }
+    cout << nline;
+}
+
+int getRandNo(int min, int max) {
+    // --------------- Random Number Generator -------------------------- //
+    random_device rd; 
+    mt19937 gen(rd()); 
+
+    uniform_int_distribution<> dist(min, max);
+
+    return dist(gen);
+}
+
+int solve(set<int> denominationsSet, int& totalAmount, unordered_map<int, int>& result) {
+
+    for (const int value: denominationsSet) {
+        int count = totalAmount/value;
+        totalAmount = totalAmount % value;
+        result[value] = count;
+    }
+
+    return totalAmount == 0;
+
+}
+
+int main() {
+
+    int totalAmount;
+    cout << "Enter the total Amount: ";
+    cin >> totalAmount;
+    vector<int> denominations;
+    unordered_map<int, int> result;
+
+    cout << "Enter the denominations: " << nline;
+
+    do {
+        int x; cin >> x; denominations.push_back(x);
+    } while  ( cin.get() != '\n');
+
+    set<int> denominationsSet(denominations.begin(), denominations.end());
+
+    int valid = solve(denominationsSet, totalAmount, result);
+
+    cout << "Denominations distributions: " << nline;
+    for (const auto it: result) {
+        cout << "Denomination " << it.first << " has count: " << it.second << nline;
+    }
+
+    if (!valid) {
+        cout << "With a spare amount of: " << totalAmount << nline;
+    }
+
+    return 0;
+
+}
+
